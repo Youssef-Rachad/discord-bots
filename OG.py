f74e92824dfcfcc0bd9d772e9f73d510e18a6c2e
Youssef-Rachad <69124029+Youssef-Rachad@users.noreply.github.com>
2021-01-01 23:04:28 -0500

    switched from client to bot, bot commands are executed with a defined prefix instead of checking message string

--- OG.py
+++ OG.py
@@ -1,5 +1,6 @@
 #import keep_alive
 import discord
+from discord.ext import commands
 import os
 import requests
 import json
@@ -9,8 +10,13 @@ from pathlib import Path
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 print(os.getenv('TOKEN'))
-client = discord.Client()
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
 
+bot = commands.Bot(command_prefix="$")
 '''
 encouragments
 '''
@@ -20,35 +26,52 @@ starter_encouragements = [
   "Hang in there.",
   "You are a great person / bot!"
 ]
+'''
+#roles
+'''
 
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
-
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
     
-@client.event
+
+@bot.event
 async def on_message(message):
-    print(message.author)
-    #print(message.author.color)
-    role = await message.guild.create_role(name="botter", color = discord.Color(0x0000FF))
-    await message.author.add_roles(role)
-    if message.author == client.user:
-        return
-    if message.content.startswith('$Hello'):
-        await message.channel.send('Hello!')
-    if message.content.startswith('$inspire'):
-      quote = get_quote()
-      await message.channel.send(quote)
     if any(word in message.content for word in sad_words):
       await message.channel.send(random.choice(starter_encouragements))
+    await bot.process_commands(message)
+@bot.command(
+	# ADDS THIS VALUE TO THE $HELP PING MESSAGE.
+	help="Uses come crazy logic to determine if pong is actually the correct value or not.",
+	# ADDS THIS VALUE TO THE $HELP MESSAGE.
+	brief="Prints pong back to the channel."
+)
+async def ping(ctx):
+	# SENDS A MESSAGE TO THE CHANNEL USING THE CONTEXT OBJECT.
+        print("ping sent")
+        await ctx.channel.send("pong")
+@bot.command(
+        name="hello",
+        help="Greetings",
+        brief="Says hello" 
+)
+async def hello(ctx):
+    print(ctx.author)
+    await ctx.channel.send("Hello {}".format(ctx.author))
+    role = discord.utils.find(lambda role: role.name == "botter", ctx.message.server.roles)
+    if role in ctx.author.roles: 
+        roleToAdd = discord.utils.get(ctx.message.author, name = "botter")
+        await bot.add_roles(ctx.author,roleToAdd)
 
+@bot.command(
+        name="inspire",
+        help="I'm here to inspire you",
+        brief="a bot to inspire you"
+        )
+async def inspire(ctx):
+    quote = get_quote()
+    await ctx.channel.send(quote)
 
 #keep_alive.keep_alive()
-
-client.run(os.getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
 
